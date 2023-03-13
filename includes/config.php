87b3a931eaa3914b1153d9eb42191222ca922d84
Emmanuelle G <79573483+xXxluckyx619@users.noreply.github.com>
2023-03-13 08:38:43 +0800

    cleaned for sharing purposes

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ ini_set('error_reporting', E_ALL);
 
 // DATABASE INFORMATION
 define('DATABASE_HOST', getenv('IP'));
-define('DATABASE_NAME', 'invoicemgsys');
+define('DATABASE_NAME', 'Bill_sender');
 define('DATABASE_USER', 'root');
 define('DATABASE_PASS', '');
 
